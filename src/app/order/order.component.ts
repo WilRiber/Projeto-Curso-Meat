98e2a968211cecfb5ce139988df46d7561192f9a
Willian Ribeiro <ribeiro3001@hotmail.com>
2018-03-22 16:04:28 -0300

    Alterar aquivo README.md

---
+++ src/app/order/order.component.ts
@@ -0,0 +1,42 @@
+import { Component, OnInit } from '@angular/core';
+
+import {RadioOption} from '../shared/radio/radio-option.model'
+
+import {OrderService} from '../order/order.service'
+
+import {CartItem} from '../restaurant-detail/shopping-cart/cart-item.model'
+
+@Component({
+  selector: 'mt-order',
+  templateUrl: './order.component.html'
+})
+export class OrderComponent implements OnInit {
+
+  paymentOptions: RadioOption[] = [
+    {label: 'Dinheiro', value: 'MON'},
+    {label: 'Cartão de Débito', value: 'DEB'},
+    {label: 'Cartão Refeição', value: 'REF'}
+  ]
+
+  constructor(private orderService: OrderService) { }
+
+  ngOnInit() {
+  }
+
+  cartItems(): CartItem[] {
+    return this.orderService.cartItems()
+  }
+
+  increaseQty(item: CartItem) {
+    this.orderService.increaseQty(item)
+  }
+
+  decreaseQty(item: CartItem){
+    this.orderService.decreaseQty(item)
+  }
+
+  remove(item: CartItem) {
+    this.orderService.remove(item)
+  }
+
+}
